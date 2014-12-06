8e1b4a78877af96974aaada0e4b4cae168b5ac7c
Victor <bocharsky.bw@gmail.com>
2014-12-06 14:21:10 +0200

    Add user sign in form

--- backend/src/BionicUniversity/Bundle/UserBundle/Controller/UserController.php
+++ backend/src/BionicUniversity/Bundle/UserBundle/Controller/UserController.php
@@ -9,6 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use BionicUniversity\Bundle\UserBundle\Entity\User;
 use BionicUniversity\Bundle\UserBundle\Form\UserType;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
 /**
  * User controller.
@@ -17,6 +18,51 @@ use BionicUniversity\Bundle\UserBundle\Form\UserType;
  */
 class UserController extends Controller
 {
+    /**
+     * @Route("/sign-in", name="user_sign_in")
+     * @Template("BionicUniversityUserBundle:User:sign-in.html.twig")
+     */
+    public function signInAction(Request $request)
+    {
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContextInterface::AUTHENTICATION_ERROR
+            );
+        } elseif (null !== $session && $session->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get(SecurityContextInterface::LAST_USERNAME);
+
+        return array(
+            // last username entered by the user
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        );
+    }
+
+    /**
+     * @Route("/sign-in-check", name="user_sign_in_check")
+     */
+    public function signInCheckAction()
+    {
+        return;
+    }
+
+    /**
+     * @Route("/sign-out", name="user_sign_out")
+     */
+    public function signOutAction()
+    {
+        return;
+    }
 
     /**
      * Lists all User entities.
