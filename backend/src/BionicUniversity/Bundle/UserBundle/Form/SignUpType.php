66e5403531b6d3b8d171d560d0962c080be6d342
Victor <bocharsky.bw@gmail.com>
2014-12-06 23:10:27 +0200

    Add sign up page with validation

---
+++ backend/src/BionicUniversity/Bundle/UserBundle/Form/SignUpType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace BionicUniversity\Bundle\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class SignUpType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', 'text')
+            ->add('password', 'password')
+            ->add('email', 'email')
+            ->add('signUp', 'submit')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BionicUniversity\Bundle\UserBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'bionicuniversity_bundle_userbundle_user';
+    }
+}
